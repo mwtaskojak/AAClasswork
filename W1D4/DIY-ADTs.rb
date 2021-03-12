7bf66220f42f34d579712fbbe9cd1a5bdd817aa0
mwtas <mwtasemalkojak@gmail.com>
2021-03-12 02:41:29 -0500

    exercise3 done

--- W1D4/DIY-ADTs.rb
+++ W1D4/DIY-ADTs.rb
@@ -43,3 +43,42 @@ class Queue
 end 
 
 # EXERCISE 3
+class Map
+
+    attr_reader :clas_array
+    def initialize
+        clas_array = []
+      end
+
+      def set(key, value)
+        pairs = clas_array.index { |pair| pair[0] == key }
+        if pairs
+          clas_array[pairs][1] = value
+        else
+          clas_array.push([key, value])
+        end
+        value
+      end
+
+
+      def get(key)
+        clas_array.each { |pair| return pair[1] if pair[0] == key }
+        nil
+      end
+
+      def delete(key)
+        value = get(key)
+        clas_array.reject! { |pair| pair[0] == key }
+        value
+      end
+    
+      def deep_dup(arr)
+        arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+      end  
+
+
+      def show
+        deep_dup(clas_array)
+      end
+
+    end 
